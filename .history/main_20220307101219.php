ca6e1571ced1a96785ca4ccc81ed3c9d83a04373
david <D00239311@student.dkit.ie>
2022-03-07 11:28:16 +0000

    first commit

---
+++ .history/main_20220307101219.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ukriane</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<!-- <link rel="stylesheet" type="text/css" href="./main.scss"/> -->
+
+<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+	<link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet" />
+	<link rel="stylesheet" type="text/css" href="main.css" />
+</head>
+<body>
+<style>
+body {
+  background-image: url('.images/083554381.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
+</style>
+<div class="container-fluid banner">
+		<div class="row">
+			<div class="col-md-12">
+				<nav class="navbar navbar-md">
+					<div class="navbar-brand">Ukraine Sign Up Form</div>
+					<ul class="nav">
+						<li class="nav-item">
+							<a class="nav-link" href="#">HOME</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">PORTFOLIO</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">ABOUT</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">CONTACT</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<div class="col-md-8 offset-md-2 info">
+				<h1 class="text-center">Ukraine Military</h1>
+				<p class="text-center">
+        “There is no flag large enough to cover the shame of killing innocent people.”
+				</p>
+				<a href="index.php" class="btn btn-md text-center">GET STARTED</a>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
