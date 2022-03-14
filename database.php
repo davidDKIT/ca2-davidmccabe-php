7505b19512b4160dd09ce976bca93153f24613ac
david <D00239311@student.dkit.ie>
2022-03-14 12:12:16 +0000

    reverting database back to local host

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=D00239311';
-    $username = 'D00239311';
-    $password = 'TKJKbRa2';
+    $dsn = 'mysql:host=localhost;dbname=ukraine_signup';
+    $username = 'root';
+    $password = '';
 
     try {
         $db = new PDO($dsn, $username, $password);
